200485f309d9f28378ef28b37dfbca298f74338e
khai <khaitruong1301@gmail.com>
2026-04-02 19:51:24 +0700

    dp

---
+++ next-sitemap.config.ts
@@ -0,0 +1,10 @@
+//tạo site map
+import { IConfig } from "next-sitemap";
+
+const config: IConfig = {
+    siteUrl: "https://shoesstorebc91.vercel.app",
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+}
+
+export default config;
